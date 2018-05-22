ae0b256a0019d37b9d595141ed07f1fae4ecdc80
trljohnson <trljohnson@mweb.co.za>
2018-05-22 16:49:02 +0200

    Project Initialisation

---
+++ src/main/java/au/com/lifebio/LifeBio/LifeBioApplication.java
@@ -0,0 +1,12 @@
+package au.com.lifebio.LifeBio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeBioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeBioApplication.class, args);
+	}
+}
