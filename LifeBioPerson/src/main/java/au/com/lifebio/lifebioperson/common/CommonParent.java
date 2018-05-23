3112cdfc42d9f191b99cb47245cd7ae6af4c4486
trljohnson <trljohnson@mweb.co.za>
2018-05-23 16:51:06 +0200

    Parent class for all beans containing standard object identifier and last modified properties.

---
+++ LifeBioPerson/src/main/java/au/com/lifebio/lifebioperson/common/CommonParent.java
@@ -0,0 +1,19 @@
+package au.com.lifebio.lifebioperson.common;
+
+/**
+ * The <code>CommonParent</code> is used as a 'container' to
+ * represent a objects state.
+ *
+ *
+ * @author <a href="mailto:inboxtrevorjohnson@gmail.com">ALR</a>
+ */
+public interface CommonParent {
+
+    void setOID(Long oID);
+
+    Long getOID();
+
+    void setLastMofified(Long lastModified);
+
+    Long getLastModified();
+}
