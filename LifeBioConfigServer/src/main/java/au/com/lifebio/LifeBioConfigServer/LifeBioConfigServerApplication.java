e4eb1b85e44e11f1acaa4e9c8a81841a6ee4bd2f
trljohnson <trljohnson@mweb.co.za>
2018-06-23 14:32:40 +0200

    Initial Commit

---
+++ LifeBioConfigServer/src/main/java/au/com/lifebio/LifeBioConfigServer/LifeBioConfigServerApplication.java
@@ -0,0 +1,14 @@
+package au.com.lifebio.LifeBioConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class LifeBioConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeBioConfigServerApplication.class, args);
+	}
+}
