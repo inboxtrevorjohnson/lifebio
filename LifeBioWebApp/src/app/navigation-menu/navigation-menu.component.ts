3768137abc6cc71bb99749614ba42dd470c1eedb
trljohnson <trljohnson@mweb.co.za>
2018-08-03 10:41:51 +0200

    Added navigation menu.

---
+++ LifeBioWebApp/src/app/navigation-menu/navigation-menu.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-menu',
+  templateUrl: './navigation-menu.component.html',
+  styleUrls: ['./navigation-menu.component.css']
+})
+export class NavigationMenuComponent implements OnInit {
+
+  isCollapsed = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
