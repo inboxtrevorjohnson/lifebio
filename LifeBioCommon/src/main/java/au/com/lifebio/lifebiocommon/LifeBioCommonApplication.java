91da880004132d74033f45c01d5505ec5e78262c
trljohnson <trljohnson@mweb.co.za>
2018-08-23 15:58:48 +0200

    Added docker files for each service and docker-compose for combined app.

---
+++ LifeBioCommon/src/main/java/au/com/lifebio/lifebiocommon/LifeBioCommonApplication.java
@@ -0,0 +1,12 @@
+package au.com.lifebio.lifebiocommon;
+
+import au.com.lifebio.lifebiocommon.common.CommonConfiguration;
+import org.springframework.boot.SpringApplication;
+
+
+public class LifeBioCommonApplication extends CommonConfiguration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeBioCommonApplication.class, args);
+    }
+}
